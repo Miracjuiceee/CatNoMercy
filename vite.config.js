9757fd8ed0d437e79d0ac0cc5fee8976cea2391a
CreativGuyMirac <108751476+Miracjuiceee@users.noreply.github.com>
2024-05-30 17:27:46 +0200

    vite config system root debug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        soon: resolve(root, "soon", "soon.html"),
+      },
+    },
   },
 });
